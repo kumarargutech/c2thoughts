fa605a7d740a555c72aab95f36b82e63b9a3a09b
Kumar Ramalingam <kramali1@visteon.com>
2019-09-27 00:25:47 +0530

    Updates

---
+++ precache-manifest.b4ab901d1a97a5df78bdf58f024f7065.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d25c9b4c30ada5d5b11b55980fe01aa",
+    "url": "/customer-connect/index.html"
+  },
+  {
+    "revision": "2938b7d2ecef92e266aa",
+    "url": "/customer-connect/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2068d5964869d3bad5d2",
+    "url": "/customer-connect/static/js/2.5233ee02.chunk.js"
+  },
+  {
+    "revision": "2938b7d2ecef92e266aa",
+    "url": "/customer-connect/static/js/main.04ec9838.chunk.js"
+  },
+  {
+    "revision": "fa9ecddccc65e3d7e558",
+    "url": "/customer-connect/static/js/runtime-main.9022f50a.js"
+  }
+]);
